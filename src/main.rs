efb889514019e189e499382227ade16f4c30bc82
uripper <36463273+uripper@users.noreply.github.com>
2023-04-12 13:19:25 -0600

    changes. Simple API to take in a json and return "beepo weepo"

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, web, HttpResponse, Responder, App, HttpServer};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct Request {
+    name: String,
+    age: u8,
+}
+
+#[post("/api")]
+async fn api(_request: web::Json<Request>) -> impl Responder {
+    HttpResponse::Ok().body(format!("Beepo weepo!"))
+}
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello, world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(api)
+            .service(index)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
