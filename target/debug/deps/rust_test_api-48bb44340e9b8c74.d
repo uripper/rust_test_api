aab532847e5ef7f7a67f76c69882ac9eb271cd9f
uripper <36463273+uripper@users.noreply.github.com>
2023-04-12 12:24:14 -0600

    First commit

---
+++ target/debug/deps/rust_test_api-48bb44340e9b8c74.d
@@ -0,0 +1,5 @@
+c:\Users\perle\Documents\GitHub\rust_test_api\target\debug\deps\rust_test_api-48bb44340e9b8c74.exe: src\main.rs
+
+c:\Users\perle\Documents\GitHub\rust_test_api\target\debug\deps\rust_test_api-48bb44340e9b8c74.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/rust_test_api-f6037884ecf07af5.d
@@ -0,0 +1,5 @@
+c:\Users\perle\Documents\GitHub\rust_test_api\target\debug\deps\rust_test_api-f6037884ecf07af5.rmeta: src\main.rs
+
+c:\Users\perle\Documents\GitHub\rust_test_api\target\debug\deps\rust_test_api-f6037884ecf07af5.d: src\main.rs
+
+src\main.rs:
